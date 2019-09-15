50734af7e208a9fe61cc97583c6b6001c9510695
Гребенщиков Адам <m1x.vk.null@gmail.com>
2019-09-15 09:21:14 +0400

    project structure added

---
+++ src/main/java/com/bellintegrator/firstTask/FirstTaskApplication.java
@@ -0,0 +1,12 @@
+package com.bellintegrator.firstTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTaskApplication.class, args);
+	}
+}
